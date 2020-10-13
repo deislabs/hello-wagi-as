72b56dcdd42a6721c37a80bf382b4db57c2c0120
Matt Butcher <matt.butcher@microsoft.com>
2020-10-13 16:10:15 -0600

    initial commit
    Signed-off-by: Matt Butcher <matt.butcher@microsoft.com>

---
+++ README.md
@@ -0,0 +1,91 @@
+# hellow-wagi: AssemblyScript example of WAGI
+
+_WAGI is the easiest way to get started doing cloud-side WASM web apps._
+
+**WARNING:** This is experimental code put together on a whim.
+It is not considered production-grade by its developers, neither is it "supported" software.
+This is a project we wrote to demonstrate another way to use WASI.
+
+> DeisLabs is experimenting with many WASM technologies right now.
+> This is one of a multitude of projects (including [Krustlet](https://github.com/deislabs/krustlet))
+> designed to test the limits of WebAssembly as a cloud-based runtime.
+
+## What It Does
+
+This is an example of WAGI. It is written in [AssemblyScript](https://www.assemblyscript.org/),
+a WebAssembly-oriented dialect of TypeScript/JavaScript.
+
+This demo exhibits how to write server-side WASM+WASI that handles HTTP requests.
+
+## Building
+
+- Clone this repo to `hello-wagi` and enter that directory.
+- Use `npm i` to install all dependencies.
+- Use `npm run asbuild` to build a WASM binary.
+
+## Configuring and Running
+
+We recommend using the [WAGI server](https://github.com/deislabs/wagi) to run this module.
+In your `modules.toml` for the WAGI server, add the following:
+
+```toml
+[[module]]
+route = "/as-hello"
+module = "/path/to/hello-wagi/build/optimized.wasm"
+```
+
+Substitute the correct module path for wherever you cloned this repo.
+
+At that point, you should be able to access this program at `http://localhost:3000/as-hello`
+(Possibly substituting in your own domain and port, depending on the WAGI server).
+
+You can use `curl` to test:
+
+```console
+$ curl -v http://localhost:3000/as-hello
+*   Trying 127.0.0.1...
+* TCP_NODELAY set
+* Connected to localhost (127.0.0.1) port 3000 (#0)
+> GET /as-hello HTTP/1.1
+> Host: localhost:3000
+> User-Agent: curl/7.64.1
+> Accept: */*
+> 
+< HTTP/1.1 200 OK
+< content-type: text-plain
+< content-length: 32
+< date: Tue, 13 Oct 2020 22:07:07 GMT
+< 
+hello world from localhost:3000
+* Connection #0 to host localhost left intact
+* Closing connection 0
+```
+
+And you can change the host and message:
+
+```console
+$ curl -v -H HOST:example.com http://localhost:3000/as-hello\?greet\=matt
+*   Trying 127.0.0.1...
+* TCP_NODELAY set
+* Connected to localhost (127.0.0.1) port 3000 (#0)
+> GET /as-hello?greet=matt HTTP/1.1
+> Host:example.com
+> User-Agent: curl/7.64.1
+> Accept: */*
+> 
+< HTTP/1.1 200 OK
+< content-type: text-plain
+< content-length: 28
+< date: Tue, 13 Oct 2020 22:08:33 GMT
+< 
+hello matt from example.com
+* Connection #0 to host localhost left intact
+* Closing connection 0
+```
+
+You can omit the `-v` to just see the response body:
+
+```console
+$ curl -H HOST:example.com http://localhost:3000/as-hello\?greet\=matt
+hello matt from example.com
+```
---
+++ assembly/index.ts
@@ -0,0 +1,76 @@
+/*
+ * This script shows how to do some simple things with WAGI and WASI.
+ *
+ * We get access to HTTP headers from the `Environ` object.
+ * We get access to query parameters from the `CommandLine` object.
+ * We write data back to the client using `Console.log`, and we write data to the error
+ * log using `Console.error`.
+ */
+import "wasi";
+import { Console, Environ, CommandLine } from "as-wasi";
+
+// Sending to Console.log is essentially sending to STDOUT. In WAGI, as in CGI, the first
+// section of the response MUST be a header. And in the header, it MUST contain either
+// 'content-type' or 'location'. If neither of those is present, it will cause a
+// 500 Internal Server Error.
+Console.log("content-type: text-plain");
+Console.log(""); // A blank line separates headers from body.
+
+// This sends a message to the server's log.
+Console.error("Module loaded.");
+
+// Any call to Console.log after this will be written to the HTTP response body and sent
+// to the client.
+
+// Get a handle to the environment variables.
+let env = new Environ();
+
+// Get the 'Host' header from the HTTP request. Note that all HTTP headers are prefixed
+// with 'HTTP_'
+let host = env.get("HTTP_HOST");
+
+// This is how we get information from the query string.
+// If the query string contains `greet=NAME`, then we will replace 'world' with the name.
+// For example, http://localhost/greet=matt will set `greet` to be "matt"
+let vars = parseArgs();
+let greet = "world";
+if (vars.has("greet")) {
+  greet = vars.get("greet");
+}
+
+// Now we will send the message hello NAME from HOST.
+// If name and host are not set, it will be "hello world from unknown host".
+Console.log("hello " + greet + " from " + defstr(host, "unknown host"));
+
+// Load the arguments and parse the query string values out of them.
+//
+// In WAGI (as in CGI), the query parameters from a query string are parsed into name/value
+// pairs and appended to the args. The first arg is the URI's path component, and the rest
+// are query params expressed as arguments.
+//
+// So http://example.com/as-env?greet=matt&foo=bar creates an arg list that looks like
+// `["/as-env", "greet=matt", "foo=bar"]`. This function parses out the name/value pairs
+// and returns a map like `{"greet": "matt", "foo": "nar"}`.
+function parseArgs(): Map<string, string> {
+  let vars = new Map<string, string>();
+  let cl = new CommandLine();
+  let args = cl.all();
+
+  // Parse all of the args into name/value pairs.
+  // We skip the first arg b/c that is just the WASM path (as in POSIX args).
+  if (args.length > 1) {
+    for (let i = 1; i < args.length; i++) {
+      let pair = args[i].split("=");
+      if (pair.length == 2) {
+        vars.set(pair[0], pair[1]);
+      }
+    }
+  }
+
+  return vars;
+}
+
+// Utility function for getting a default string.
+function defstr(input: string | null, defaultValue: string): string {
+  return input === null ? defaultValue : input;
+}
